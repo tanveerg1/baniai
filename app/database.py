910cf8e68252a65b6bbe51624f8abb4682519544
tanveerg1 <ghatourha.tanveer@gmail.com>
2025-05-16 16:19:20 -0400

    new changes

--- app/database.py
+++ app/database.py
@@ -9,7 +9,7 @@ db_password = os.getenv("MONGO_DB_PASSWORD")
 cluster_name = os.getenv("MONGO_DB_CLUSTER_NAME")
 app_name = os.getenv("MONGO_DB_APP_NAME")   
 
-MONGODB_URI = f"mongodb+srv://{db_username}:{db_password}@baniaidb.{cluster_name}.mongodb.net/"  # Replaced by env var on Render
+MONGODB_URI = f"mongodb+srv://{db_username}:{db_password}@{cluster_name}.mongodb.net/"  # Replaced by env var on Render
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
